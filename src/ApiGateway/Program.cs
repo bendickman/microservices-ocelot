27be6a04e011d928c03ec743e8cd4f1d1bf5e72f
Ben Dickman <Ben.Dickman@connellsgroup.co.uk>
2024-09-17 13:06:03 +0100

    Swagger for Ocelot

--- src/ApiGateway/Program.cs
+++ src/ApiGateway/Program.cs
@@ -1,19 +1,45 @@
+using ApiGateway.Config;
 using JwtAuthenticationManager.Extensions;
+using MMLib.SwaggerForOcelot.DependencyInjection;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Provider.Polly;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+var routes = "Routes";
+
+builder.Configuration.AddOcelotWithSwaggerSupport(options =>
+{
+    options.Folder = routes;
+});
+
+builder.Services.AddOcelot(builder.Configuration).AddPolly();
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
+
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
+    .AddOcelot(routes, builder.Environment)
     .AddEnvironmentVariables();
 
-builder.Services.AddOcelot(builder.Configuration);
+
 builder.Services.AddCustomJwtAuthentication();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-await app.UseOcelot();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+}
+
+app.UseSwaggerForOcelotUI(options =>
+{
+    options.PathToSwaggerGenerator = "/swagger/docs";
+    options.ReConfigureUpstreamSwaggerJson = AlterUpstream.AlterUpstreamSwaggerJson;
+
+}).UseOcelot().Wait();
 
 app.UseAuthentication();
 app.UseAuthorization();
